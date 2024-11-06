78b2312b160480894da2a4d2e3037a704d96906b
Tarek Siddique Nabil <nabilh420@gmail.com>
2024-11-06 22:48:31 +0600

    📝 Pointers

--- pointers/main.go
+++ pointers/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
+
+	var p Person = Person{Name: "Alice", Age: 30}
+	fmt.Println(p.Name, p.Age)
+	p.Age = 50
+	fmt.Println(p.Name, p.Age)
 
 }
